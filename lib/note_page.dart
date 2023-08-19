e5f895d7fa7de1576b706ab9d299453954a46a4e
大白兔奶糖 <285292107@qq.com>
2023-08-19 17:54:30 +0800

    0.5

--- lib/note_page.dart
+++ lib/note_page.dart
@@ -20,56 +20,59 @@ class _NotePageState extends State<NotePage> {
 
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
-  FilePickerResult? _filePickerResult;
-  File? _file;
-  String? _filePath;
+  late Future<bool> openTheLastDocument;
+  String? path;
 
-//读取文档内容
-  _loadFile() async {
-    try {
-      //debugPrint("$_filePath");
-      if (_filePath == null) {
-        await getSingleFile();
-        _filePath = _filePickerResult!.files.single.path;
-        debugPrint("_filePickerResult.files.single.path");
-      }
-      _file = File(_filePath!);
-      if (await _file!.exists()) {
-        controller.text = await _file!.readAsString();
-        debugPrint("file.readAsString");
-      }
-    } catch (e) {
-      debugPrint(e.toString());
+  //读取文档内容
+  _readAsString(String path) async {
+    File file = File(path);
+    if (await file.exists()) {
+      controller.text = await file.readAsString();
+      debugPrint("file.readAsString");
     }
+    setState(() {});
   }
 
-//获取路径
-  getSingleFile() async {
-    _filePickerResult = await FilePicker.platform.pickFiles(
+  //获取选择器返回结果
+  Future<FilePickerResult?> pickTextFile() async {
+    FilePickerResult? filePickerResult = await FilePicker.platform.pickFiles(
         lockParentWindow: true,
         type: FileType.custom,
         allowedExtensions: ['txt']);
-    _filePath = _filePickerResult!.files.single.path;
-    var prefs = await _prefs;
-    prefs.setString("filePath", _filePath!);
-    debugPrint("${_filePickerResult!.files.single.path}");
+
+    if (filePickerResult != null) {
+      //及时写入本地存储
+      var prefs = await _prefs;
+      prefs.setString("filePath", filePickerResult.files.single.path!);
+      setState(() {
+        debugPrint("重绘，更新底部文字");
+        path = filePickerResult.files.single.path;
+      });
+    }
+
+    return filePickerResult;
+  }
+
+  //读取上次打开的文档
+  Future<bool> _openTheLastDocument() async {
+    SharedPreferences prefs = await _prefs;
+    path = prefs.getString('filePath');
+    //path = null;
+    if (path == null) {
+      setState(() {
+        _visible = true;
+      });
+      return false;
+    } else {
+      _readAsString(path!);
+      return true;
+    }
   }
 
   @override
   void initState() {
     super.initState();
-    () async {
-      //读取上次打开的文档
-      var prefs = await _prefs;
-      if (_filePath == null) {
-        await _loadFile();
-        _filePath = _filePickerResult!.files.single.path;
-        prefs.setString("filePath", _filePath!);
-      } else {
-        _filePath = prefs.getString("filePath");
-      }
-      setState(() {});
-    };
+    openTheLastDocument = _openTheLastDocument();
   }
 
   @override
@@ -86,27 +89,33 @@ class _NotePageState extends State<NotePage> {
         children: <Widget>[
           //const Text('标题'),
           Expanded(
-            child: _file == null
-                ? const Text("loading……")
-                : TextField(
-                    maxLines: null,
-                    expands: true,
-                    focusNode: focusNode,
-                    controller: controller,
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      enabledBorder: InputBorder.none,
-                      contentPadding: EdgeInsets.fromLTRB(20, 12, 20, 12),
-                    ),
-                    onChanged: (text) {
-                      setState(() {});
-                    },
-                    onTap: () {
-                      debugPrint("隐藏按钮");
-                      setState(() => _visible = false);
-                    },
-                  ),
+            child: FutureBuilder(
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return TextField(
+                      maxLines: null,
+                      expands: true,
+                      focusNode: focusNode,
+                      controller: controller,
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        contentPadding: EdgeInsets.fromLTRB(20, 12, 20, 12),
+                      ),
+                      onChanged: (text) {
+                        setState(() {});
+                      },
+                      onTap: () {
+                        debugPrint("隐藏按钮");
+                        setState(() => _visible = false);
+                      },
+                    );
+                  } else {
+                    return const Text("加载中……");
+                  }
+                },
+                future: openTheLastDocument),
           ),
           MouseRegion(
             onEnter: (_) => setState(() => _visible = true),
@@ -114,7 +123,7 @@ class _NotePageState extends State<NotePage> {
             child: SizedBox(
               width: double.infinity,
               child: Center(
-                child: Text(_filePath == null ? "loading" : _filePath!),
+                child: path == null ? const Text("加载中……") : Text(path!),
               ),
             ),
           )
@@ -133,11 +142,12 @@ class _NotePageState extends State<NotePage> {
                 const SizedBox(width: 10),
                 FloatingActionButton(
                   onPressed: () {
-                    debugPrint("保存");
+                    debugPrint("打开");
                     () async {
-                      debugPrint("保存2");
-                      await getSingleFile();
-                      _loadFile();
+                      String? path = await pickTextFile()
+                          .then((value) => value?.files.single.path);
+                      _readAsString(path!);
+                      setState(() {});
                     }();
                   },
                   tooltip: "打开",
